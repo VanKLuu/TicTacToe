c90d922c4a92d9f911c1f417fc16eb7b0069285b
aedwardsC <118393110+aedwardsC@users.noreply.github.com>
2024-04-15 14:46:44 -0700

    Base Code with Modification

--- library/src/tic_tac_toe/game/engine.py
+++ library/src/tic_tac_toe/game/engine.py
@@ -1,24 +1,22 @@
-from __future__ import annotations
-
 from dataclasses import dataclass
 from typing import Callable, TypeAlias
-
 from tic_tac_toe.game.players import Player
 from tic_tac_toe.game.renderers import Renderer
 from tic_tac_toe.logic.exceptions import InvalidMove
 from tic_tac_toe.logic.models import GameState, Grid, Mark
 from tic_tac_toe.logic.validators import validate_players
 
+# for letting the front end display helpful errors
 ErrorHandler: TypeAlias = Callable[[Exception], None]
-
 
 @dataclass(frozen=True)
 class TicTacToe:
     player1: Player
     player2: Player
-    renderer: Renderer
+    renderer: Renderer # responsible for visualizing the grid
     error_handler: ErrorHandler | None = None
 
+    # validate the marks being placed
     def __post_init__(self):
         validate_players(self.player1, self.player2)
 
@@ -35,8 +33,10 @@ class TicTacToe:
                 if self.error_handler:
                     self.error_handler(ex)
 
+    # mapping the current player to their assigned mark
     def get_current_player(self, game_state: GameState) -> Player:
         if game_state.current_mark is self.player1.mark:
             return self.player1
         else:
             return self.player2
+        
