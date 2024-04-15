71e205bcca21739ad01adfc70cfa049901aa315a
aedwardsC <118393110+aedwardsC@users.noreply.github.com>
2024-04-15 15:48:38 -0700

    Implement 4x4

--- frontends/play.py
+++ frontends/play.py
@@ -1,11 +1,12 @@
 from tic_tac_toe.game.engine import TicTacToe
-from tic_tac_toe.game.players import MinimaxComputerPlayer
+from tic_tac_toe.game.players import MinimaxComputerPlayer, RandomComputerPlayer
 from tic_tac_toe.logic.models import Mark
 
 from console.renderers import ConsoleRenderer
 from console.players import ConsolePlayer
 
 player1 = ConsolePlayer(Mark("X"))
-player2= MinimaxComputerPlayer(Mark("O"))
+player2= RandomComputerPlayer(Mark("O")) # for testing purposes until implement eval function
+# player2= MinimaxComputerPlayer(Mark("O"))
 
 TicTacToe(player1, player2, ConsoleRenderer()).play()
