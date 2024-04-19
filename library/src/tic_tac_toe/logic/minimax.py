e09b3676fde22d751ed42874591606c4d88f3d42
aedwardsC <118393110+aedwardsC@users.noreply.github.com>
2024-04-18 18:23:31 -0700

    Implemented Alpha Beta Pruning

--- library/src/tic_tac_toe/logic/minimax.py
+++ library/src/tic_tac_toe/logic/minimax.py
@@ -2,38 +2,50 @@ from functools import partial
 from tic_tac_toe.logic.models import GameState, Mark, Move
 
 
-def minimax(game_state: GameState, maximizer: Mark, is_maximizing: bool, depth: int = 0) -> int:
+def minimax(game_state: GameState, maximizer: Mark, is_maximizing: bool, depth: int = 0, alpha: float = float('-inf'), beta: float = float('inf')) -> int:
     if depth >= 4 or game_state.game_over:
         return game_state.evaluate_score(maximizer)
 
     if is_maximizing:
         best_score = float('-inf')
         for move in game_state.possible_moves:
-            score = minimax(move.after_state, maximizer, False, depth + 1)
+            score = minimax(move.after_state, maximizer, False, depth + 1, alpha, beta)
             best_score = max(best_score, score)
+            alpha = max(alpha, best_score)
+            if alpha >= beta:
+                break
         return best_score
     else:
         best_score = float('inf')
         for move in game_state.possible_moves:
-            score = minimax(move.after_state, maximizer, True, depth + 1)
+            score = minimax(move.after_state, maximizer, True, depth + 1, alpha, beta)
             best_score = min(best_score, score)
+            beta = min(beta, best_score)
+            if alpha >= beta:
+                break
         return best_score
     
-def reverse_minimax(game_state: GameState, minimizer: Mark, is_minimizing: bool, depth: int = 0) -> int:
+def reverse_minimax(game_state: GameState, minimizer: Mark, is_minimizing: bool, depth: int = 0, alpha: float = float('-inf'), beta: float = float('inf')) -> int:
     if depth >= 4 or game_state.game_over:
         return game_state.evaluate_score(minimizer)
     
     if is_minimizing:
         worst_score = float('inf')
         for move in game_state.possible_moves:
-            score = reverse_minimax(move.after_state, minimizer, False, depth + 1)
+            score = reverse_minimax(move.after_state, minimizer, False, depth + 1, alpha, beta)
             worst_score = min(worst_score, score)
+            beta = min(beta, worst_score)
+            if alpha >= beta:
+                break
         return worst_score
     else:
         worst_score = float('-inf')
         for move in game_state.possible_moves:
-            score = reverse_minimax(move.after_state, minimizer, True, depth + 1)
+            score = reverse_minimax(move.after_state, minimizer, True, depth + 1, alpha, beta)
             worst_score = max(worst_score, score)
+            alpha = max(alpha, worst_score)
+            if alpha >= beta:
+                break
         return worst_score
 
 def find_best_move(game_state: GameState) -> Move:
